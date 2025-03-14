c6967f369187b8aaa2969ab34a67784bb9e5d354
Alexandru Carbunaru <alex.carbunaru@gmail.com>
2025-03-14 13:31:14 +0200

    Update Budget table to include the Period column

---
+++ PersonalFinanceTrackerDataAccess/Migrations/20250314110426_AddPeriodColumnToBudgetTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinanceTrackerDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPeriodColumnToBudgetTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Period",
+                table: "Budgets",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "Budgets");
+        }
+    }
+}
